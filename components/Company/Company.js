706a1f328db5132a9d02e99710b7335555d8a8cc
Zack Seuberling <zack.seuberling@leafly.com>
2020-01-26 13:35:10 -0800

    add css and markup for companies

--- components/Company/Company.js
+++ components/Company/Company.js
@@ -1,12 +1,33 @@
+import css from "./company.css";
+
 function Company({ company }) {
   const [name, url, size, expertise, twitter, linkedin] = company;
+  const services = expertise.split(",");
 
   return (
-    <div>
-      {[name, url, size, expertise, twitter, linkedin]
-        .filter(Boolean)
-        .join(",")}
-    </div>
+    <article className={css.company}>
+      <header className={css.header}>
+        <h1 className={css.name}>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {name}
+          </a>
+        </h1>
+        <div className={css.size}>{size}</div>
+      </header>
+
+      <div className={css.meta}>
+        <div className={css.services}>
+          <h6>Services</h6>
+          {services.map(service => (
+            <span key={service} className={css.service}>
+              {service}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <div className={css.social}>{twitter}</div>
+    </article>
   );
 }
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
-module.exports = {
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
   env: {
     hostname: process.env.SELF_HOSTNAME
+  },
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]"
   }
-};
+});
