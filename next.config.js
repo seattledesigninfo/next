22d80e2c28ad06160679f0186ea8b8610f64b889
Zack Seuberling <seuberling@gmail.com>
2020-08-07 09:44:39 -0700

    add next-offline

--- next.config.js
+++ next.config.js
@@ -1,11 +1,33 @@
 require("dotenv").config();
-
 const withCSS = require("@zeit/next-css");
+const withOffline = require("next-offline");
 
-module.exports = withCSS({
+const nextConfig = {
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: "[local]___[hash:base64:5]",
   },
-});
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = withCSS(withOffline(nextConfig));
